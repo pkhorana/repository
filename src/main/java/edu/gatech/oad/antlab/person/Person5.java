6b5d7788b7ac75bbc259eb7bf7d45cf5c5b51f52
Bryan Cook <bcook75@gatech.edu>
2019-01-24 14:20:42 -0500

    Bryan Cook Commit 1

--- src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -1,49 +1,67 @@
 package edu.gatech.oad.antlab.person;
 
 /**
- *  A simple class for person 5
- *  returns their name and a
- *  modified string 
- *  
- *  @author Bob
- *  @version 1.1
+ * A simple class for person 5
+ * returns their name and a
+ * modified string
+ *
+ * @author Bob
+ * @version 1.1
  */
 public class Person5 {
-  /** Holds the persons real name */
-  private String name;
-  	/**
-	 * The constructor, takes in the persons
-	 * name
-	 * @param pname the person's real name
-	 */
-  public Person5(String pname) {
-    name = pname;
-  }
-  	/**
-	 * This method should take the string
-	 * input and return its characters rotated
-	 * 2 positions.
-	 * given "gtg123b" it should return
-	 * "g123bgt".
-	 *
-	 * @param input the string to be modified
-	 * @return the modified string
-	 */
-	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
-	}
-	
-	/**
-	 * Return a string rep of this object
-	 * that varies with an input string
-	 *
-	 * @param input the varying string
-	 * @return the string representing the 
-	 *         object
-	 */
-	public String toString(String input) {
-	  return name + calc(input);
-	}
+    /**
+     * Holds the persons real name
+     */
+    private String name;
+
+    /**
+     * The constructor, takes in the persons
+     * name
+     *
+     * @param pname the person's real name
+     */
+    public Person5(String pname) {
+        name = pname;
+    }
+
+    /**
+     * This method should take the string
+     * input and return its characters rotated
+     * 2 positions.
+     * given "gtg123b" it should return
+     * "g123bgt".
+     *
+     * @param input the string to be modified
+     * @return the modified string
+     */
+    private String calc(String input) {
+        if(input == null){
+            throw new NullPointerException("Input data cannot be null");
+        }
+        String newString = "";
+        if(input.length() <= 2){
+            newString = input;
+        } else {
+            for (int i = 2; i < input.length(); i++) {
+                newString += input.charAt(i);
+            }
+            newString += input.charAt(0);
+            newString += input.charAt(1);
+
+        }
+        return newString;
+    }
+
+    /**
+     * Return a string rep of this object
+     * that varies with an input string
+     *
+     * @param input the varying string
+     * @return the string representing the
+     * object
+     */
+    public String toString(String input) {
+        return name + calc(input);
+    }
 
 }
