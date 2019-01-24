99e2eedff8efa46f1f569b383272e3e6530dadd5
Bryan Cook <bcook75@gatech.edu>
2019-01-24 18:02:13 -0500

    Bryan Cook Commit 2 AntLabMain change p5

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -74,7 +74,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("Bryan Cook");
-                  toPrint += p5.toString("903188637");
+                  toPrint += p5.toString("bcook75");
 		  
         System.out.println(toPrint);
         
